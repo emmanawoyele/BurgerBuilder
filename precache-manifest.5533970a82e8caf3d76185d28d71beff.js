badbef0eb1cff975b493bc434cf1bc86a3876cb4
Manny <emmanawoyele@gmail.com>
2020-12-31 10:30:09 +0000

    Updates

---
+++ precache-manifest.5533970a82e8caf3d76185d28d71beff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e8e5f576c93b7e0c4f84e59f2b4782",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "85068e7bedd486786752",
+    "url": "/BurgerBuilder/static/css/main.6878f13c.chunk.css"
+  },
+  {
+    "revision": "703c1b682ea31784a912",
+    "url": "/BurgerBuilder/static/js/2.547fee01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BurgerBuilder/static/js/2.547fee01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85068e7bedd486786752",
+    "url": "/BurgerBuilder/static/js/main.1bca5a9b.chunk.js"
+  },
+  {
+    "revision": "5dce666f3f7679fab113",
+    "url": "/BurgerBuilder/static/js/runtime-main.c431f402.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/original.b8503d26.png"
+  }
+]);
